c975380e97891498297b59c051c94336be1bacc5
Sebastian Livoni <seb@livoni.me>
2021-08-16 20:11:08 +0200

    Responsive layout v1

--- js/script.js
+++ js/script.js
@@ -22,15 +22,19 @@ $(window).scroll(function() {
 
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height() - 50){
-            $("#navbar a").css({"color":"#1A1A1D"});   
-            $("#navbar h3").css({"color":"#1A1A1D"});   
-        }
-        else{
+        if($(window).width() > 1200) {
+            if($(window).scrollTop() > $(window).height() - 50){
+                $("#navbar a").css({"color":"#1A1A1D"});   
+                $("#navbar h3").css({"color":"#1A1A1D"});   
+            }
+            else{
+                $("#navbar a").css({"color":"white"});
+                $("#navbar h3").css({"color":"white"});
+            }
+        } else {
             $("#navbar a").css({"color":"white"});
             $("#navbar h3").css({"color":"white"});
         }
-
     })
 })
 
