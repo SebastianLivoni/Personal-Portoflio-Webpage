c4a47c79040e5c11bdd9c5a11fedfd4fa354642d
Sebastian Livoni <livoni@Sebs-MacBook-Pro.local>
2021-08-16 17:04:32 +0200

    Added My Work, About Me and updated some design fails

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(window).height() - 50){
+            $("#navbar a").css({"color":"#1A1A1D"});   
+            $("#navbar h3").css({"color":"#1A1A1D"});   
+        }
+        else{
+            $("#navbar a").css({"color":"white"});
+            $("#navbar h3").css({"color":"white"});
+        }
+
+    })
+})*/
+
+/* 
+- Tooltips
+- Scroll to top
+- Navbar color switch 
+*/
