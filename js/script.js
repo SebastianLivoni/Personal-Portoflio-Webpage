5448e531c82e621d6ea2712b61ea5868b57584e0
Sebastian Livoni <livoni@Sebs-MacBook-Pro.local>
2021-08-16 17:39:02 +0200

    Navbar jquery animation scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//Navbar scroll animation
+
+$("a").click(function() {
+    var href = $(this).attr('href');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(href).offset().top
+    }, 250);
+});
+
 /*$(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > $(window).height() - 50){
@@ -15,5 +24,6 @@
 /* 
 - Tooltips
 - Scroll to top
-- Navbar color switch 
+- Navbar color switch
+- Navbar scroll to animation 
 */
