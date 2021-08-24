4490950fc6db419cf5543ee15d28b77c652947a2
Sebastian Livoni <seb@livoni.me>
2021-08-24 22:22:30 +0200

    h2 and h4 now using typewriter js

--- js/typeeffect.js
+++ js/typeeffect.js
@@ -1,19 +1,40 @@
 // Typewriter
 
-var app = document.getElementById('test');
+var h2 = document.getElementById('typewriter-h2');
 
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 50,
-  deleteSpeed: 40,
+var typewriter = new Typewriter(h2, {
+  loop: false,
+  delay: 75,
+  deleteSpeed: 60,
 });
 
 typewriter
+  //.pauseFor(1000)
+  .typeString("Hey. ")
+  //.typeString("<h4>an upcoming full stack developer.</h4>")
   .pauseFor(1000)
-  .typeString('an upcoming full stack developer.')
-  .pauseFor(600)
-  .deleteChars(32)
-  .typeString(' <strong>19-year-old</strong> dude from Denmark.')
+  .typeString("I'm Sebastian.")
+  //.deleteChars(32)
+  //.typeString(' <strong>19-year-old</strong> dude from Denmark.')
   //.typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-  .pauseFor(1000)
+  //.pauseFor(1000)
+  .start();
+
+var h4 = document.getElementById('typewriter-h4');
+
+var typewriter = new Typewriter(h4, {
+  loop: false,
+  delay: 75,
+  deleteSpeed: 60,
+});
+
+typewriter
+  //.pauseFor(1000)
+  //.typeString("<h4>an upcoming full stack developer.</h4>")
+  .pauseFor(3000)
+  .typeString("an upcoming full stack developer.")
+  //.deleteChars(32)
+  //.typeString(' <strong>19-year-old</strong> dude from Denmark.')
+  //.typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  //.pauseFor(1000)
   .start();
