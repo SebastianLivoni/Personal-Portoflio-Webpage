44f07c6ede48c9233120b67b95ba06bbbcdabcd7
Sebastian Livoni <seb@livoni.me>
2021-08-24 23:15:36 +0200

    changed mouseanimation.css to animations.css and added custom hamburger icon to navbar toggler + js for it

--- js/script.js
+++ js/script.js
@@ -18,37 +18,14 @@ $(window).scroll(function() {
     }
 });
 
-/*$(window).on('scroll', function(){
-    var s = $(window).scrollTop(),
-        d = $(document).height(),
-        c = $(window).height();
+// Hamburger
 
-    var scrollPercent = (s / (d - c)) * 100;
-
-    console.clear();
-    console.log(scrollPercent);
-})*/
-
-// navbar color changing
-/*
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).width() > 1200) {
-            if($(window).scrollTop() > $(window).height() - 50){
-                $("#navbar a").css({"color":"#1A1A1D"});   
-                $("#navbar h3").css({"color":"#1A1A1D"});   
-            }
-            else{
-                $("#navbar a").css({"color":"white"});
-                $("#navbar h3").css({"color":"white"});
-            }
-        } else {
-            $("#navbar a").css({"color":"white"});
-            $("#navbar h3").css({"color":"white"});
-        }
-    })
-})
-
-/* 
-- Navbar color switch
-*/
+$('.menu').click(function() {
+	if($(this).is('.active:not(.back)')) {
+		 $(this).addClass('back');
+	} else if ($(this).is('.back')) {
+		$(this).removeClass('back');
+	} else {
+		$(this).addClass('active');
+	}
+});
